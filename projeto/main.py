2613edaf88c9df080d06410615e6a519020fb09a
Erick Santos <erick.santos.devops@gmail.com>
2023-07-05 22:57:40 -0300

    add date2

--- projeto/main.py
+++ projeto/main.py
@@ -1,5 +1,6 @@
 #permit a comment
 """Don't is comment, but
 can use for this finality"""
-print ('05', '07', '2023', sep='/')
+print ('Date of this day', end=':')
+print ('05', '07', '2023.', sep='/')
 print ('Hello, world!')
