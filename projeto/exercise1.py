0efb13fe53cc7ea6c80b9cc1bdd959a8eb823191
Erick Santos <erick.santos.devops@gmail.com>
2023-07-06 13:44:53 -0300

    add exercise1

---
+++ projeto/exercise1.py
@@ -0,0 +1,12 @@
+name = "Erick"
+last_name = "Santos"
+year_old = 27
+year_of_birth = 1996
+of_legal_age = year_old >= 18
+height_in_meters = 1.75
+print ('Name:', name)
+print ('Last name:', last_name)
+print ('Year old:', year_old)
+print ('Year of birth:', year_of_birth)
+print ('Of legal age?', of_legal_age)
+print ('Heigh in meters:', height_in_meters)
