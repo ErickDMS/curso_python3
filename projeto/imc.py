2db752579b1f699cb6d50df24c867a78473a3900
Erick Santos <erick.santos.devops@gmail.com>
2024-05-09 11:12:05 -0300

    update2

--- projeto/imc.py
+++ projeto/imc.py
@@ -1,8 +1,10 @@
-nome = 'Erick'
+nome = 'Erick Santos'
 altura = 1.75
 peso = 85
 imc = peso/(altura*altura)
+linha= f'{nome} tem {altura} de altura e pesa {peso}kg, seu IMC é:{imc}'
 print ('Nome: ', nome)
 print ('Peso: ', peso)
 print ('Altura: ', altura)
 print ('O IMC é: ', imc)
+print (linha)
