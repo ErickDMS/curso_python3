0370ab28cd771080ac1b2e3af6c09a5801f4518f
Erick Santos <erick.santos.devops@gmail.com>
2024-05-10 12:07:07 -0300

    up2

---
+++ projeto/condicionais.py
@@ -0,0 +1,9 @@
+entrada = input ('Você que "entrar"ou "sair"? ')
+if entrada == 'entrar' :
+    print('Você escolheu entrar.')
+elif entrada == 'sair' :
+    print ('Você escolheu sair.')
+else:
+    print ('Você escolheu nenhum.')
+
+print('FORA DO IF')
