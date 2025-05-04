56cf1ac31d75ceda8adc51d2fe03a3f34d6d40eb
Erick Santos <erick.santos.devops@gmail.com>
2025-05-04 15:44:50 -0300

    e isso ai

---
+++ projeto/operadores.py
@@ -0,0 +1,9 @@
+maior = 3 > 1
+maior_igual = 3>= 3
+menor = 1 < 3
+menor_igual = 1 <= 1
+diferente = 11 != 13
+igual = 20 == 20
+
+if (maior and maior_igual and menor and menor_igual and diferente and igual==True):
+    print("Todas sao verdadeiras")
