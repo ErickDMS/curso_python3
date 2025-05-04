e750cbfa69cc78799b6b0bac034439134381b240
Erick Santos <erick.santos.devops@gmail.com>
2025-05-04 15:58:34 -0300

    v1

---
+++ projeto/exercise2.py
@@ -0,0 +1,12 @@
+primeiro_numero = input("Digite o primeiro numero: ")
+segundo_numero = input("Digite o segundo numero: ")
+if primeiro_numero >= segundo_numero:
+    print (
+        f'{primeiro_numero} E maior ou igual'
+        f'ao {segundo_numero}'
+    )
+else:
+    print (
+        f'{primeiro_numero} E menor ou igual'
+        f' ao {segundo_numero}'
+    )
