767b13c54d601793082250e3b743d881627c3468
Erick Santos <erick.santos.devops@gmail.com>
2025-05-05 22:39:30 -0300

    v3.1

--- projeto/formatacao.py
+++ projeto/formatacao.py
@@ -4,8 +4,8 @@ print (f'{variavel: >10}') #10 espacos para direita
 print (f'{variavel: <10}') #10 espacos para esquerda
 print (f'{variavel: ^10}') #10 no centro de 10 espacos
 print (f'{1000.32452345: ,.2f}')
-
-
+print (f'O hexadecimal de 1500 e {1500:08X}')
+print (f'{variavel!r}')
 
 
 
