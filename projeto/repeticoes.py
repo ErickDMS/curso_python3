31ea1dda6e61a90d844cbd4c5931bf170b674f2f
Erick Santos <erick.santos.devops@gmail.com>
2025-05-07 22:07:34 -0300

    5v.2

--- projeto/repeticoes.py
+++ projeto/repeticoes.py
@@ -1,9 +1,15 @@
-entrada = input('Digite um numero:')
+entrada = 20
 entrada_int = int(entrada)
 
 print(f'entrada_int e {entrada_int}')
 
-while entrada_int <= 90:
-    entrada_int=entrada_int + 1
-    print('Acrescentado mais 1')
-    print(f'Valor atual {entrada_int}')
+while entrada_int <= 80:
+    print(f'Valor {entrada_int}')
+    entrada_int += 1
+    if entrada_int == 40:
+        print('??')
+        continue
+    if entrada_int >= 50 and entrada_int <=60:
+        print('??')
+        continue
+    
