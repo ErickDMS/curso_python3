6a5a809c218b3eea50f011466fe7b45db6208c9e
Erick Santos <erick.santos.devops@gmail.com>
2025-05-08 21:37:58 -0300

    6v

---
+++ projeto/calculadora.py
@@ -0,0 +1,46 @@
+
+while True:
+
+    numero1 = input('Digite um numero: ')
+    numer02 = input('Digite outro numero: ')
+    operador = input('Escolha o operador + - * /:')
+    numeros_validos = None
+
+    try:
+        num1_float = float(numero1)
+        num2_float = float(numer02)
+        numeros_validos = True
+    except:
+        if numeros_validos is None:
+            print('Um dos numeros e invalido')
+            continue
+
+    operador_permitido = '+-*/'
+
+    if operador not in operador_permitido:
+        print('Operador invalido')
+        continue
+    if len(operador) > 1:
+        print('Digite apenas um operador')
+        continue
+
+    if operador == '*':
+        resultado = num1_float * num2_float
+        print(f'Resultado: {resultado}')
+    elif operador == '+':
+        resultado = num1_float + num2_float
+        print(f'Resultado: {resultado}') 
+    elif operador == '-':
+        resultado = num1_float - num2_float
+        print(f'Resultado: {resultado}')   
+    elif operador == '/':
+        resultado = num1_float / num2_float
+        print(f'Resultado: {resultado}')
+    else:
+        print('Algo de errado nao esta certo.')
+
+    sair = input('Voce quer sair? [s]').lower().startswith('s')
+    if sair:
+        print('Good bye')
+        break
+    
