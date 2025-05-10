f6f607be2f49646e1dc86f15035cfe2d7ad0aecb
Erick Santos <erick.santos.devops@gmail.com>
2025-05-09 23:33:46 -0300

    v7

--- projeto/exercise4.py
+++ projeto/exercise4.py
@@ -2,9 +2,16 @@ frase = ' O Python e uma linguagem de programacao '\
     'multparadgma '\
     'criada por Guido Van Rossum'
 i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
 while i < len(frase):
     letra_atual = frase[i]
-    quantas_vezes_apareceu = frase.count(letra_atual)
-    print(letra_atual, quantas_vezes_apareceu) 
+    qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
+        qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
+
+    print(letra_atual, qtd_apareceu_mais_vezes_atual) 
     
     i+= 1
