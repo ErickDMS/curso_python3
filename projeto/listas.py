58c104305df57074032c2d6135c91c62174ab66c
Erick Santos <erick.santos.devops@gmail.com>
2025-05-14 22:28:19 -0300

    v1.3

--- projeto/listas.py
+++ projeto/listas.py
@@ -1,11 +1,17 @@
 lista = []
 while True:
-    variavel1 = input('Digite algo: ')
+    variavel1 = input('Digite um numero: ')
     lista = ['']
     lista[0] = variavel1
-    variavel2 = input('Digite outro algo: ')
+    variavel2 = input('Digite outro numero: ')
     lista += ['']
     lista[1] = variavel2
     print(lista)
     break
-    
+print('Outro meio de adicionar')
+lista.append(50)## Adiciona no final
+lista.pop()## Apaga o final
+variavel3 = input('Digite mais algum numero: ')
+if lista[1] < variavel3:
+    lista.append(variavel3)
+    print(lista)
