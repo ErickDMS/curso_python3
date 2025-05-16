4092cde50f9a93670bc76f2ed8ccc27201496f7a
Erick Santos <erick.santos.devops@gmail.com>
2025-05-15 23:45:20 -0300

    v1.4

--- projeto/listas.py
+++ projeto/listas.py
@@ -1,4 +1,5 @@
 lista = []
+lista_teste = ['abc', 'defg']
 while True:
     variavel1 = input('Digite um numero: ')
     lista = ['']
@@ -10,8 +11,11 @@ while True:
     break
 print('Outro meio de adicionar')
 lista.append(50)## Adiciona no final
-lista.pop()## Apaga o final
+lista.pop()## Apaga o final ou um indice escolhido
+lista.insert(0, 4) #adiciona algo em um indice selecionado
 variavel3 = input('Digite mais algum numero: ')
 if lista[1] < variavel3:
     lista.append(variavel3)
     print(lista)
+lista.extend(lista_teste) ## uma forma de concatenar
+print(lista)
