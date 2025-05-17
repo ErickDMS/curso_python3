f5941e2466e8738024a0d27a942c1ea2bdca48c4
Erick Santos <erick.santos.devops@gmail.com>
2025-05-16 21:40:59 -0300

    v1.7

---
+++ projeto/empacotamento.py
@@ -0,0 +1,14 @@
+##uma lista pode ser desempacotada dessa forma
+## nome = ['Erick', 'David', 'Santos']
+nome1, nome2, *resto = 'Erick', 'David', 'Santos'
+
+print(resto)
+## uma tuple e uma lista imutavel
+## uma lista tuple se faz assim 
+nome = 'erick', 'david', 'santos'
+## ou pode ser feito isso
+nome5 = ['erick', 'david', 'santos']
+nome5 = tuple(nome5)
+print(nome5)
+
+
