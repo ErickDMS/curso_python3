ad869aeee4ab841f23bb1769b55174026f4d73a0
Ericvk <erick.santos.devops@gmail.com>
2025-05-22 21:30:59 -0300

    v2.2.4

--- projeto/funcoes.py
+++ projeto/funcoes.py
@@ -2,3 +2,8 @@ def imprimir(nome = input('Digit seu nome: ')):
     print(nome)
 imprimir('Noberto')
 imprimir()
+def soma(x, y):
+    print (f'{x=} {y=} | x + y =', x / y)
+
+soma(5, 6)
+soma(6, 5) ##na inversao dos parametros os resultados podem ficar diferentes
