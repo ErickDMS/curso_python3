29aa7cd8e21a47ce5e3b835ec3f4ad906f5d37ad
Ericvk <erick.santos.devops@gmail.com>
2025-05-27 22:40:35 -0300

    v3

---
+++ projeto/classdict.py
@@ -0,0 +1,12 @@
+pessoa = {
+    'nome': 'Erick Santos',
+    'idade': 29,
+    'altura': 1.75,
+    'endereco': [{
+        'rua': 'Labaxuia', 'numero': 350, 'Cidade': 'Biralandia'
+    }]
+}
+
+
+for chave in pessoa:
+    print (chave, pessoa[chave])
